8bbed7b704d444e2a235e27377a093cdea673f92
andrew8822 <silverpigcg@gmail.com>
2025-04-10 14:16:41 +0800

    model conversion

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import dns.rdatatype
 from tensorflow.keras.models import load_model
 from sentence_transformers import SentenceTransformer
 import os
+import tensorflow
 
 app = Flask(__name__)
 
@@ -16,4 +17,8 @@ MODEL_PATH = os.path.join(BASE_DIR, "new_st2_model.h5")
 model = load_model(MODEL_PATH)
 model.summary()  # Optional: print the model summary
 
-model.save('model.keras')
+converter = tensorflow.lite.TFLiteConverter.from_saved_model(model)
+tflite_model = converter.convert()
+
+with open("model.tflite", "wb") as f:
+    f.write(tflite_model)
